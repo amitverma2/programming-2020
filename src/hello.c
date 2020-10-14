7eda4b118ec3c26e54a087360bd25871d8afaa9b
Amit Verma <amit.verma2@gmail.com>
2020-10-15 02:03:41 +0530

    fixed hello

--- src/hello.c
+++ src/hello.c
@@ -3,7 +3,7 @@
 int main(int argc, char *argv[])
 {
     printf("Hello, world!\n");
-    int a[3] = { [0...1]=3 };
+    int a[3] = { 3 };
     printf("%d %d %d\n", a[0], a[1], a[2]);
     return 0;
 }
