cc7f3a07e8253ce5e348b551755af02f39ee93a0
Amit Verma <amit.verma2@gmail.com>
2021-03-15 09:57:01 +0530

    merge sort again

--- basic-algorithms/sorting/merge-sort/merge-sort.cpp
+++ basic-algorithms/sorting/merge-sort/merge-sort.cpp
@@ -49,12 +49,63 @@ class MyMergeSort {
     }
 };
 
+// Top Down 2
+class Solution {
+    void mergeSortArray(vector<int>& nums, int first, int last) {
+        if (first >= last) return;
+        int mid = first + (last - first)/2;
+        mergeSortArray(nums, first, mid);
+        mergeSortArray(nums, mid+1, last);
+        vector<int> mergedVector(last-first+1);
+        int to = 0; int from1 = first; int from2 = mid+1;
+        while (from1 <= mid && from2 <= last) {
+            if (nums[from1] <= nums[from2])
+                mergedVector[to++] = nums[from1++];
+            else
+                mergedVector[to++] = nums[from2++];
+        }
+        while (from1 <= mid) {
+            mergedVector[to++] = nums[from1++];
+        }
+        while (from2 <= last) {
+            mergedVector[to++] = nums[from2++];
+        }
+        for (int i = 0 ; i < (last-first+1); ++i) {
+            nums[first+i] = mergedVector[i];
+        }
+        return;
+    }
+public:
+    void print(vector<int> & sequence) {
+      cout << "Sequence is :\n" ;
+      for (vector<int>::const_iterator p = sequence.begin();
+           p != sequence.end();
+           ++p) {
+             cout << *p << " ";
+           }
+      cout << endl;
+    }
+    vector<int> sortArray(vector<int>& nums) {
+        if (nums.size() > 1) {
+            mergeSortArray(nums, 0, nums.size()-1);
+        }
+        return nums;
+    }
+};
+
 int main() {
   vector<int> sequence = { 11, 2, 3, 14 };
 
+/*
   MyMergeSort * obj = new MyMergeSort;
   obj->print(sequence);
   obj->sort(sequence, 0, sequence.size()-1);
   obj->print(sequence);
+*/
+  Solution * sol = new Solution;
+  sol->print(sequence);
+  sol->sortArray(sequence);
+  sol->print(sequence);
+
   return 0;
 }
