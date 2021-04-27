0eaac7a159f51949cc531699c8416204154195ff
Amit Verma <amit.verma2@gmail.com>
2021-04-28 03:51:06 +0530

    my own hacked kernel in c. missing few things.

---
+++ os-tutorial/13-my-barebones-kernel/mykernel.h
@@ -0,0 +1,8 @@
+#ifndef __MYKERNEL_H__
+#define __MYKERNEL_H__
+
+#define VGA_DISPLAY_MEMORY_START    (0xB8000)
+#define VGA_ROWS                    (25)
+#define VGA_COLS                    (80)
+#define VGA_DISPLAY_FLAG            (0x0F)
+#endif // __MYKERNEL_H__
