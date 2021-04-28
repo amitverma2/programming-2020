7d3f7e8b6afd59a0d3deb5946832508e5464c7d6
Amit Verma <amit.verma2@gmail.com>
2021-04-29 01:33:11 +0530

    Got the build setup with makefiles done

--- os-tutorial/13-my-barebones-kernel/mykernel.c
+++ os-tutorial/13-my-barebones-kernel/mykernel.c
@@ -1,6 +1,18 @@
 #include "mykernel.h"
 
-static void clear_screen(void);
+static void clear_screen(void)
+{
+    char space = ' ';
+    char flag = 0x00;
+    char * mem = (char *)VGA_DISPLAY_MEMORY_START;
+    for (int r = 0; r < 25; r++) {
+        for (int c = 0 ; c < 80; ++c) {
+            *(mem++) = space;
+            *(mem++) = flag;
+        }
+    }
+    return;
+}
 
 void my_kernel_entry(void)
 {
@@ -12,20 +24,6 @@ void my_kernel_entry(void)
     for (int i = 0 ; i < sizeof(msg)/sizeof(msg[0]) - 1; ++i) {
         *(mem++) = msg[i];
         *(mem++) = VGA_DISPLAY_FLAG;
-    }
-    return;
-}
-
-static void clear_screen(void)
-{
-    char space = ' ';
-    char flag = 0x00;
-    char * mem = (char *)VGA_DISPLAY_MEMORY_START;
-    for (int r = 0; r < 25; r++) {
-        for (int c = 0 ; c < 80; ++c) {
-            *(mem++) = space;
-            *(mem++) = flag;
-        }
     }
     return;
 }
---
+++ os-tutorial/13-my-barebones-kernel/mykernel_entry.asm
@@ -0,0 +1,4 @@
+[bits 32]
+[extern my_kernel_entry]
+call my_kernel_entry
+jmp $
